2dc8991d47e697d9e57939e09663a25883a5d442
Zhao Guangjian <zgj0315@gmail.com>
2022-11-15 01:43:59 +0800

    format path

--- cp2tonic.sh
+++ cp2tonic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
-cp home-ca/sub-ca.crt ../../hyperium/tonic/examples/data/tls/ca.pem
+cp home-ca/crts/sub-ca.crt ../../hyperium/tonic/examples/data/tls/ca.pem
 cp home-ca/private/dj-ca.key ../../hyperium/tonic/examples/data/tls/server.key
-cp home-ca/dj-ca.crt ../../hyperium/tonic/examples/data/tls/server.pem
+cp home-ca/crts/dj-ca.crt ../../hyperium/tonic/examples/data/tls/server.pem
