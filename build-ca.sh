b1277e7a8e019ffa8619f78c0de56dab90dd0c2b
Zhao Guangjian <zgj0315@gmail.com>
2022-11-14 22:56:24 +0800

    change path

--- build-ca.sh
+++ build-ca.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 home_ca="home-ca"
 rm -rf $home_ca
-mkdir $home_ca
-cd $home_ca
-mkdir certs db private
-chmod 700 private
-touch db/index
-openssl rand -hex 16 >db/serial
-echo 1001 >db/crlnumber
+mkdir -p $home_ca/certs $home_ca/db $home_ca/private
+chmod 700 $home_ca/private
+touch $home_ca/db/index
+openssl rand -hex 16 >$home_ca/db/serial
+echo 1001 >$home_ca/db/crlnumber
 
 # Root CA Generation
 openssl req -nodes \
     -x509 \
     -days 3650 \
     -newkey rsa:4096 \
-    -keyout private/root-ca.key \
-    -out root-ca.crt \
+    -keyout $home_ca/private/root-ca.key \
+    -out $home_ca/root-ca.crt \
     -sha256 \
     -batch \
     -subj "/CN=Root CA"
@@ -23,37 +21,37 @@ openssl req -nodes \
 # Subordinate CA Generation
 openssl req -nodes \
     -newkey rsa:3072 \
-    -keyout private/sub-ca.key \
-    -out sub-ca.csr \
+    -keyout $home_ca/private/sub-ca.key \
+    -out $home_ca/sub-ca.csr \
     -sha256 \
     -batch \
     -subj "/CN=Sub CA"
 
 openssl x509 -req \
-    -in sub-ca.csr \
-    -out sub-ca.crt \
-    -CA root-ca.crt \
-    -CAkey private/root-ca.key \
+    -in $home_ca/sub-ca.csr \
+    -out $home_ca/sub-ca.crt \
+    -CA $home_ca/root-ca.crt \
+    -CAkey $home_ca/private/root-ca.key \
     -sha256 \
     -days 3650 \
     -set_serial 123 \
-    -extensions sub_ca_ext -extfile ../build-ca.conf
+    -extensions sub_ca_ext -extfile build-ca.conf
 
 # DJ CA Generation
 openssl req -nodes \
     -newkey rsa:2048 \
-    -keyout private/dj-ca.key \
-    -out dj-ca.csr \
+    -keyout $home_ca/private/dj-ca.key \
+    -out $home_ca/dj-ca.csr \
     -sha256 \
     -batch \
     -subj "/CN=DJ CA"
 
 openssl x509 -req \
-    -in dj-ca.csr \
-    -out dj-ca.crt \
-    -CA sub-ca.crt \
-    -CAkey private/sub-ca.key \
+    -in $home_ca/dj-ca.csr \
+    -out $home_ca/dj-ca.crt \
+    -CA $home_ca/sub-ca.crt \
+    -CAkey $home_ca/private/sub-ca.key \
     -sha256 \
     -days 2000 \
     -set_serial 456 \
-    -extensions dj_ca_ext -extfile ../build-ca.conf
+    -extensions dj_ca_ext -extfile build-ca.conf
